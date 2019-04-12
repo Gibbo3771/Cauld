38f09be5d99943bcf9a6e24dd54d14190a916f04
user <gibson.stephen92@gmail.com>
2019-04-12 11:45:47 +0100

    Babel and webpack configured

---
+++ hw/src/js/index.js
@@ -0,0 +1,12 @@
+export default class App {
+    constructor(){
+        console.log('Created!');
+    };
+
+    someFunction () {
+        console.log('Do some stuff');
+    };
+
+};
+
+// export default App;
