d5265c79c552285ac94efc546162fee3c0c44be0
Stephen Gibson <gibson.stephen92@gmail.com>
2019-04-28 14:26:34 +0100

    changed forecastday to pure function

--- hw/src/components/Forecast/Forecast.js
+++ hw/src/components/Forecast/Forecast.js
@@ -1,33 +1,23 @@
 import Component from "../../components/Component";
 import { html } from "lit-html";
 import { repeat } from "lit-html/directives/repeat";
-import ForecastDay from "../ForecastDay/ForecastDay";
+import { ForecastDay } from "../ForecastDay/ForecastDay";
 
 export default class Forecast extends Component {
   render(props) {
     super.render(props);
-    const { available } = this.props;
+    const { available, current, forecast } = this.props;
     if (!available) return;
-    const days = this.createForecastDays();
     return html`
       <div id="forecast" class="weather">
-        ${repeat(days, (day, index) =>
-          day.render({
+        ${repeat(forecast, (day, index) =>
+          ForecastDay({
             current: this.props.current,
-            forecast: this.props.forecast[index],
+            forecast: day,
             isToday: index === 0
           })
         )}
       </div>
     `;
   }
-
-  createForecastDays = () => {
-    const { current, forecast } = this.props;
-    let days = [];
-    for (let x = 0; x < forecast.length; x++) {
-      days.push(new ForecastDay());
-    }
-    return days;
-  };
 }
--- hw/src/components/ForecastDay/ForecastDay.js
+++ hw/src/components/ForecastDay/ForecastDay.js
@@ -1,111 +1,99 @@
-import {
-  getMonthString,
-  getDayString,
-  prettyDateParse
-} from "../../helpers/date_parser";
+import { getDayString, prettyDateParse } from "../../helpers/date_parser";
 import anime from "animejs";
 import { html, render } from "lit-html";
 import { classMap } from "lit-html/directives/class-map";
-import Component from "../Component";
 
-export default class ForecastDay extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render = props => {
-    super.render(props);
-    const { date } = this.props.forecast;
-    const { isToday } = this.props;
-    const classes = {
-      blue: true,
-      "current-day": isToday
-    };
-    const markup = html`
-      <div class=${classMap(classes)}>
-        <div id="header">
-          <div>
-            ${this.renderHeader()}
-          </div>
-        </div>
-        ${this.renderCurrentTemp()}
-        <div class="icon">
-          ${this.renderIcon()}
-        </div>
-        <div class="weather-descriptor">
-          ${this.renderWeatherDescriptor()}
-        </div>
-        <span class="temperature-range" }>
-          ${this.renderTempRange()}
-        </span>
-        <div class="wind">
-          ${isToday ? this.renderWind() : this.renderWindMax()}
+export const ForecastDay = props => {
+  const { date } = props.forecast;
+  const { isToday } = props;
+  const classes = {
+    blue: true,
+    "current-day": isToday
+  };
+  const markup = html`
+    <div class=${classMap(classes)}>
+      <div id="header">
+        <div>
+          ${renderHeader(props)}
         </div>
       </div>
-    `;
-    return markup;
-  };
-
-  renderHeader = () => {
-    ("header");
-    const { date, descriptor } = this.props.forecast;
-    return html`
-      <h1>${getDayString(new Date(date).getDay())}</h1>
-      <h3>${prettyDateParse(new Date(date)).descriptor}</h3>
-    `;
-  };
-
-  renderIcon = () => {
-    const { icon } = this.props.forecast;
-    return html`
-      <img src="http:${icon}" />
-    `;
-  };
-
-  renderWeatherDescriptor = () => {
-    const { descriptor } = this.props.forecast;
-    return html`
-      <p>${descriptor}</p>
-    `;
-  };
-
-  renderTempRange = () => {
-    const { minTempC, maxTempC } = this.props.forecast;
-    const classes = {
-      "temperature-range": true
-    };
-    return html`
-      ${minTempC}c°<sub>Lo</sub> ${maxTempC}c°<sub>Hi</sub>
-    `;
-  };
-
-  renderCurrentTemp = () => {
-    const { isToday } = this.props;
-    if (!isToday) return;
-    const { tempC } = this.props.current;
-    const classes = {
-      "temp-current": true
-    };
-    return html`
-      <div class=${classMap(classes)}>
-        <p>${tempC}c°</p>
+      ${renderCurrentTemp(props)}
+      <div class="icon">
+        ${renderIcon(props)}
       </div>
-    `;
-  };
+      <div class="weather-descriptor">
+        ${renderWeatherDescriptor(props)}
+      </div>
+      <span class="temperature-range" }>
+        ${renderTempRange(props)}
+      </span>
+      <div class="wind">
+        ${isToday ? renderWind(props) : renderWindMax(props)}
+      </div>
+    </div>
+  `;
+  return markup;
+};
 
-  renderWind = () => {
-    const { windMPH } = this.props.current;
-    return html`
-      <h4>Wind</h4>
-      <p>${windMPH}mph</p>
-    `;
-  };
+const renderHeader = props => {
+  ("header");
+  const { date, descriptor } = props.forecast;
+  return html`
+    <h1>${getDayString(new Date(date).getDay())}</h1>
+    <h3>${prettyDateParse(new Date(date)).descriptor}</h3>
+  `;
+};
 
-  renderWindMax = () => {
-    const { maxWindMPH } = this.props.forecast;
-    return html`
-      <h4>Wind Max</h4>
-      <p>${maxWindMPH}mph</p>
-    `;
+const renderIcon = props => {
+  const { icon } = props.forecast;
+  return html`
+    <img src="http:${icon}" />
+  `;
+};
+
+const renderWeatherDescriptor = props => {
+  const { descriptor } = props.forecast;
+  return html`
+    <p>${descriptor}</p>
+  `;
+};
+
+const renderTempRange = props => {
+  const { minTempC, maxTempC } = props.forecast;
+  const classes = {
+    "temperature-range": true
   };
-}
+  return html`
+    ${minTempC}c°<sub>Lo</sub> ${maxTempC}c°<sub>Hi</sub>
+  `;
+};
+
+const renderCurrentTemp = props => {
+  const { isToday } = props;
+  if (!isToday) return;
+  const { tempC } = props.current;
+  const classes = {
+    "temp-current": true
+  };
+  return html`
+    <div class=${classMap(classes)}>
+      <p>${tempC}c°</p>
+    </div>
+  `;
+};
+
+const renderWind = props => {
+  const { windMPH } = props.current;
+  return html`
+    <h4>Wind</h4>
+    <p>${windMPH}mph</p>
+  `;
+};
+
+const renderWindMax = props => {
+  const { maxWindMPH } = props.forecast;
+  return html`
+    <h4>Wind Max</h4>
+    <p>${maxWindMPH}mph</p>
+  `;
+};
