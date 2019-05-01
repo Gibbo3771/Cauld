e60b4657f74a9ee887a499726ce0c84d6babe682
Stephen Gibson <gibson.stephen92@gmail.com>
2019-05-01 01:05:46 +0100

    sorta went nuts with color scheme but looks alright

---
+++ hw/client/src/components/Footer/Footer.js
@@ -0,0 +1,30 @@
+import { html } from "lit-html";
+import { classMap } from "lit-html/directives/class-map";
+import store from "../../state/index";
+
+export const Footer = () => {
+  return html`
+    <div class="footer">${renderIcon()}</div>
+  `;
+};
+
+const renderIcon = () => {
+  const classes = {
+    fab: true,
+    "fa-github": true,
+    "footer-link-git": true
+  };
+  return html`
+    <div class="footer">
+      <a class=${classMap(classes)}></a>
+      <a
+        class="footer-link-atrr"
+        href="https://www.apixu.com/"
+        title="Free
+          Weather API"
+      >
+        Powered by Apixu.com</a
+      >
+    </div>
+  `;
+};
--- hw/client/src/components/List/List.js
+++ hw/client/src/components/List/List.js
@@ -2,6 +2,7 @@ import Component from "../Component";
 import { html } from "lit-html";
 import { ListItem } from "./ListItem";
 import store from "../../state/index";
+import { classMap } from "lit-html/directives/class-map";
 
 export default class List extends Component {
   constructor() {
@@ -10,9 +11,13 @@ export default class List extends Component {
 
   render() {
     const { autoCompleteVisible } = store.state;
-    if (!autoCompleteVisible) return;
+    if (!autoCompleteVisible || store.state.locations.length === 0) return;
+    const classes = {
+      "autocomplete-items": true,
+      "autocomplete-items-bordered": true
+    };
     return html`
-      <div id="container" class="autocomplete-items">
+      <div id="container" class=${classMap(classes)}>
         ${this.createItems()}
       </div>
     `;
