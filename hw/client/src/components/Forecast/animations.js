a2f553a3a6c346910dcfebfcce072105a21d6745
Stephen Gibson <gibson.stephen92@gmail.com>
2019-05-04 17:15:02 +0100

    fixed timing and removed for loop in animation

--- hw/client/src/components/Forecast/animations.js
+++ hw/client/src/components/Forecast/animations.js
@@ -2,20 +2,12 @@ import anime from "animejs";
 
 export const animate = (target, delay) => {
   const elements = document.querySelectorAll(".day-background");
-  elements.forEach((el, index) => {
-    anime({
-      targets: el,
-      delay: (index + 1) * 100,
-      duration: 1000,
-      easing: "easeOutBack",
-      translateY: -50
-    });
-    anime({
-      targets: el,
-      opacity: 1,
-      delay: (index + 1) * 150,
-      duration: 200,
-      easing: "linear"
-    });
+  anime({
+    targets: elements,
+    delay: anime.stagger(100),
+    opacity: 1,
+    duration: 250,
+    easing: "easeOutBack",
+    translateY: -50
   });
 };
